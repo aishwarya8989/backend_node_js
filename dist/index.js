59f9427eaf3473d48c788cff4e2555846772bb39
aishwarya8989 <aishwaryabeniwal89@gmail.com>
2024-11-06 17:11:07 +0530

    chenges

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 8080;
 app.get("/", (req, res) => {
     console.log("hellloooooo");
+    res.send("hellllooooo")
 });
 app.listen(port, () => {
     console.log("server started");
