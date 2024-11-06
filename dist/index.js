2d65557a0e96d2582cc7fc9ad10c1c3712171047
aishwarya8989 <aishwaryabeniwal89@gmail.com>
2024-11-06 17:01:42 +0530

    adding routes

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 const app = express();
 const port = 8080;
+app.get("/", (req, res) => {
+    console.log("hellloooooo");
+});
 app.listen(port, () => {
     console.log("server started");
 });
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,11 @@ const app: Application=express();
 
 const port:Number= 8080
 
+app.get("/",(req,res)=>{
+console.log("hellloooooo");
+
+})
+
 app.listen(port,()=>{
     console.log("server started");
     
